6ca776c129f912d62a064eafb56d498828b3bfd2
simon-happy <959875329@qq.com>
2023-02-19 20:25:15 +0800

    sdf

--- src/pages/blog/new.jsx
+++ src/pages/blog/new.jsx
@@ -2,21 +2,61 @@ import {Component} from "react";
 import styles from "./new.module.css";
 
 export default class PageNewPost extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            title: "",
+            summary: "",
+            content: "",
+            formStatus: 0
+        }
+    }
+
+    _onTitleChange = (e) => {
+        this.setState({
+            title: e.target.value
+        })
+    }
+    _onSummaryChange = (e) => {
+        this.setState({
+            summary: e.target.value
+        })
+    }
+    _onContentChange = (e) => {
+        this.setState({
+            content: e.target.value
+        })
+    }
+
+    _handleSubmit = (e) => {
+        e.preventDefault();
+        this.setState({
+            formStatus: 1
+        })
+        console.log(`title: ${this.state.title}\nsummary: ${this.state.summary}\ncontent:${this.state.content}`)
+    }
+
     render() {
         return (
             <main>
-                <form className={styles.form}>
-                    <h1>创建一篇新的博客</h1>
+                <form onSubmit={this._handleSubmit} className={styles.form}>
+                    <h1>创建一篇新的博文</h1>
                     <label htmlFor="" className={styles.titleLabel}>标题
-                        <input className={styles.titleInput} type="text" placeholder="输入一个标题" required/>
+                        <input value={this.state.title} onChange={this._onTitleChange} className={styles.titleInput}
+                               type="text" placeholder="输入一个标题" required/>
                     </label>
                     <label htmlFor="" className={styles.summaryLabel}>摘要
-                        <input className={styles.summaryInput} type="text" placeholder="输入一个摘要" required/>
+                        <input value={this.state.summary} onChange={this._onSummaryChange}
+                               className={styles.summaryInput} type="text" placeholder="输入一个摘要" required/>
                     </label>
                     <label htmlFor="" className={styles.contentLabel}>内容
-                        <textarea placeholder="输入主体内容" className={styles.contentTextarea} name="content" id="content" rows="10"></textarea>
+                        <textarea value={this.state.content} onChange={this._onContentChange} placeholder="输入主体内容"
+                                  className={styles.contentTextarea} name="content"
+                                  id="content" rows="10"></textarea>
                     </label>
-                </form>                
+                    <button disabled={this.state.formStatus !== 0} type="submit" className={styles.submitButton}>提交
+                    </button>
+                </form>
             </main>
         )
     }
